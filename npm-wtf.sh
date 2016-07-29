4b7df352f63ec495973ae7f0d679ea1cf838e5fa
Amo Wu <amo260@gmail.com>
2016-07-29 13:58:46 +0800

    add npm-wtf.sh

---
+++ npm-wtf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Usage: ./npm-wtf.sh express
+
+regex="'?([a-zA-Z-]+)'?:"
+for dependency in $(npm view $1 dependencies); do
+  if [[ ${dependency} =~ $regex ]]; then
+    echo "${BASH_REMATCH[1]} - $(npm view ${BASH_REMATCH[1]} description)"
+  fi
+done
