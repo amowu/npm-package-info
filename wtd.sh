31718d86d240ba964b31b645f2c122b0072f881f
Amo Wu <amo260@gmail.com>
2016-07-29 17:09:14 +0800

    remove shell script

--- wtd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Usage: ./wtd.sh express
-
-regex="'?([a-zA-Z-]+)'?:"
-for dependency in $(npm view $1 dependencies); do
-  if [[ ${dependency} =~ $regex ]]; then
-    echo "${BASH_REMATCH[1]} - $(npm view ${BASH_REMATCH[1]} description)"
-  fi
-done
