19e638fc4eb0ccb73316e05d0be8685c9c8ab38e
Amo Wu <amo260@gmail.com>
2016-07-30 00:38:23 +0800

    feat: add $ wtd npm <name>

--- src/lib/parser/npm.js
+++ src/lib/parser/npm.js
@@ -1,37 +1,43 @@
+import 'babel-polyfill' // Support async and await function
+import fetch from 'node-fetch'
+
 /**
  * npm dependencies parser
- * @param  {string} name Package name
- * @param  {Object} options
- * @param  {boolean} options.listUrl Whether show repo URL
- * @return {string} Dependencies info
+ * @param {string} name
+ * @param {{listUrl: string}} options
+ * @return {Promise.<{{name: string, description: string}}[], Error>}
  */
-module.exports = function (name, options) {
-  return `
-  accepts - Higher-level content negotiation
-  array-flatten - Flatten nested arrays
-  content-disposition - Create and parse Content-Disposition header
-  content-type - Create and parse HTTP Content-Type header
-  cookie - HTTP server cookie parsing and serialization
-  cookie-signature - Sign and unsign cookies
-  debug - small debugging utility
-  depd - Deprecate all the things
-  encodeurl - Encode a URL to a percent-encoded form, excluding already-encoded sequences
-  escape-html - Escape string for use in HTML
-  etag - Create simple ETags
-  finalhandler - Node.js final http responder
-  fresh - HTTP response freshness testing
-  merge-descriptors - Merge objects using descriptors
-  methods - HTTP methods that node supports
-  on-finished - Execute a callback when a request closes, finishes, or errors
-  parseurl - parse a url with memoization
-  path-to-regexp - Express style path to RegExp utility
-  proxy-addr - Determine address of proxied request
-  qs - A querystring parser that supports nesting and arrays, with a depth limit
-  range-parser - Range header field string parser
-  send - Better streaming static file server with Range and conditional-GET support
-  serve-static - Serve static files
-  type-is - Infer the content-type of a request.
-  utils-merge - merge() utility function
-  vary - Manipulate the HTTP Vary header
-  `
+module.exports = async function (name, options) {
+  try {
+    // example: $ wtd npm express
+    const packageJSON = await fetchJSON(`https://registry.npmjs.org/${name}/latest`)
+    // packageJSON = { name: 'express', dependencies: { 'accepts': 'x.x.x', ... }, ... }
+    const { dependencies } = packageJSON
+    // dependencies = { 'accepts': 'x.x.x', ... }
+    const fetchList = Object.keys(dependencies).map(dependency => {
+      const version = dependencies[dependency]
+      return fetchJSON(`https://registry.npmjs.org/${dependency}/${version}`)
+    })
+    // fetchList = [fetchJSON(...), fetchJSON(...), ...]
+    const resultList = await Promise.all(fetchList)
+    // resultList = [{ name, description, version, author, ...}, {...}, ...]
+    return resultList.map(result => {
+      // only return you need, [{ name, description}, {...}, ...]
+      const { name, description } = result
+      return {
+        name,
+        description
+      }
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function fetchJSON (url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(res => res.json())
+      .then(json => resolve(json))
+  })
 }
